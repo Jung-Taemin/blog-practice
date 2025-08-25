8a40286a8df2dae08a74780fd247a486c5fc1677
taemin <jungtaemin1214@naver.com>
2025-08-25 15:04:22 +0900

    changes

--- src/main/java/me/jungtaemin/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/jungtaemin/springbootdeveloper/controller/UserApiController.java
@@ -1,9 +1,13 @@
 package me.jungtaemin.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.jungtaemin.springbootdeveloper.dto.AddUserRequest;
 import me.jungtaemin.springbootdeveloper.service.UserService;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -15,6 +19,13 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                securityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
