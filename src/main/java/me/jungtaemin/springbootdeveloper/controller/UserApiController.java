f12c9d2dcf0c5ccb526d379d05e31068670b3f7e
taemin <jungtaemin1214@naver.com>
2025-08-25 14:25:51 +0900

    changes

---
+++ src/main/java/me/jungtaemin/springbootdeveloper/controller/UserApiController.java
@@ -0,0 +1,4 @@
+package me.jungtaemin.springbootdeveloper.controller;
+
+public class UserApiController {
+}
---
+++ src/main/java/me/jungtaemin/springbootdeveloper/service/UserService.java
@@ -0,0 +1,23 @@
+package me.jungtaemin.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.jungtaemin.springbootdeveloper.domain.User;
+import me.jungtaemin.springbootdeveloper.dto.AddUserRequest;
+import me.jungtaemin.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
