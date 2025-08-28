b0e58ebd37e0aeaae2bd840dabd0581bfe391692
taemin <jungtaemin1214@naver.com>
2025-08-28 15:13:26 +0900

    changes

--- src/test/java/me/jungtaemin/springbootdeveloper/config/jwt/TokenProviderTest.java
+++ src/test/java/me/jungtaemin/springbootdeveloper/config/jwt/TokenProviderTest.java
@@ -1,4 +1,33 @@
 package me.jungtaemin.springbootdeveloper.config.jwt;
 
+import me.jungtaemin.springbootdeveloper.domain.User;
+import me.jungtaemin.springbootdeveloper.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+@SpringBootTest
 public class TokenProviderTest {
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @DisplayName("generateToken(): 유저 정보와 만료 기간을 전달해 토큰을 만들 수 있다.")
+    @Test
+    void generateToken() {
+        // given
+        User testUser = userRepository.save(User.builder()
+                .email("user@gmail.com")
+                .password("test"))
+                .build());
+
+        // when
+        String token = tokenProvier.generateToken(testUser, Duration.ofDays(14));
+    }
 }
